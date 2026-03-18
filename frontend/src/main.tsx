e7e8c9d27d0561de095b5d344871422220ac90c2
safabm7121 <safa26069777@gmail.com>
2026-03-18 15:56:52 +0100

    ios touch

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './i18n';
@@ -35,7 +34,6 @@ if (savedTheme === 'dark') {
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
+ 
 );
