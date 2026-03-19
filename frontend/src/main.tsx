f12c84ea9cc69fa0a3b4e2da27b5bc3c43f9e331
safabm7121 <safa26069777@gmail.com>
2026-03-19 14:16:38 +0100

    carroussel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './i18n';
@@ -34,6 +35,7 @@ if (savedTheme === 'dark') {
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
- 
+  </React.StrictMode>
 );
